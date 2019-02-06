1524c18aaab8694f497658b43703c9d7b6e0bfa5
Azarielle <sharon.manrique2@hotmail.com>
2019-02-05 21:50:04 -0500

    clicked card now "pops" up

--- src/index.js
+++ src/index.js
@@ -7,18 +7,22 @@ import card2 from './assets/card2.png';
 class NavBar extends React.Component {
     render() {
         return (
-            <div className="nav-bar"></div>
+            <div className="nav-bar">
+                <h1> Yu Gi Oh </h1>
+            </div>
         )
     }
 }
 class Card extends React.Component {
     render() {
         return (
-            <div className="card-grid-item"
-                onClick={() => { alert('Click'); }}
+            <div
+                className="card-grid-item"
+                onClick={() => this.props.onClick()}
             >
+                {this.props.value}
                 <img src={card2} width={230} height={330} />
-            </div>
+            </div >
         )
     }
 }
@@ -33,28 +37,42 @@ class ActiveCard extends React.Component {
     }
 }
 
-
 class DisplayCards extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            createCards: CreateCards(),
+            activeCard: false
         };
     }
     handleClick() {
+        let activeCard = !this.state.activeCard;
+        activeCard = this.setState({ activeCard: <ActiveCard /> })
+
+    }
+
+    renderCard() {
+        const cardArr = []
+        for (let i = 0; i < 9; i++) {
+            cardArr.push(<Card
+                key={i}
+                value={this.state.activeCard}
+                onClick={() => this.handleClick(i)}
+            />)
+        }
+
+        return cardArr
 
     }
     render() {
         return (
             <div className="card-container">
                 <div className="card-grid">
-                    {this.state.createCards}
+                    {this.renderCard()}
                 </div>
 
             </div>
         )
     }
-
 }
 
 function App() {
@@ -68,15 +86,6 @@ function App() {
 
 // TODO: Iterate over each card id. (card2, card3, card5, etc) 2 3 5 2 3 5 2 3 5
 // Parameters == Props?
-function CreateCards() {
-    const cardArr = []
-
-    for (let i = 0; i < 25; i++) {
-        cardArr.push(<Card key={i} value={i} />)
-    }
-
-    return cardArr
-}
 
 ReactDOM.render(
     <App />,
